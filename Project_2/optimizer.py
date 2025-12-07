314cfe3306011efecb4e7893c416ddc4d2532b27
egouldmarie <e.gould.marie@gmail.com>
2025-12-07 15:19:18 -0700

    Add structure for inference graph for register allocation, and starting dead code elimination

--- Project_2/optimizer.py
+++ Project_2/optimizer.py
@@ -1,3 +1,12 @@
+class InferenceGraph:
+    def __init__(self):
+        self.nodes = set()
+        self.edges = {}
+    
+    def addEdge(self, node1, node2):
+        self.edges[node1] = node2
+        self.edges[node2] = node1
+
 class Optimizer:
     def __init__(self, cfg):
         """
@@ -7,12 +16,14 @@ class Optimizer:
         Args:
             cfg: A Control Flow Graph object.
         """
+        self.cfg = cfg
+
         # Initialize LV_in set, and LV_out set
         self.IN = {}
         self.OUT = {}
         self.GEN = {}
         self.KILL = {}
-        for node in cfg.nodes:
+        for node in self.cfg.nodes:
             self.IN[node.label] = set()
             self.OUT[node.label] = set()
             self.GEN[node.label] = self.gen(node)
@@ -21,7 +32,7 @@ class Optimizer:
         # Print Live Variable Analysis Equations
         print(f"LV_in(l)  = gen(l) ∪ (LV_out(l) / kill(l))")
         print(f"LV_out(l) = U LV_in(l') | l' ∈ l's successors\n")
-        for node in cfg.nodes:
+        for node in self.cfg.nodes:
             gen = self.GEN[node.label] if len(self.GEN[node.label]) > 0 else "∅"
             kill = self.KILL[node.label] if len(self.KILL[node.label]) > 0 else "∅"
             print(f"label_{node.label}: {node.content}")
@@ -41,8 +52,8 @@ class Optimizer:
         changed = True
         while changed:
             changed = False
-            for n in range(len(cfg.nodes)-1, -1, -1):
-                node = cfg.nodes[n]
+            for n in range(len(self.cfg.nodes)-1, -1, -1):
+                node = self.cfg.nodes[n]
                 old_out = self.OUT[node.label]
                 old_in = self.IN[node.label]
                 self.LVA_out(node)
@@ -54,13 +65,16 @@ class Optimizer:
         # Print Live Variable In and Out sets for each node in the CFG
         print(f"Live variable analysis completed in {iteration} iteration(s).")
         print("Results:\n")
-        for node in cfg.nodes:
+        for node in self.cfg.nodes:
             print(f"label_{node.label}: {node.content}")
             _in = self.IN[node.label] if len(self.IN[node.label]) > 0 else "∅"
             _out = self.OUT[node.label] if len(self.OUT[node.label]) > 0 else "∅"
             print(f"LV_in({node.label})  = {_in}")
             print(f"LV_out({node.label}) = {_out}")
             print("")
+        
+        # Eliminate dead code using Live Variable sets
+        self.eliminate_dead_code()
 
     def LVA_out(self, cfg_node):
         if cfg_node.label != "exit":
@@ -94,3 +108,35 @@ class Optimizer:
             get_gen_var(cfg_node.ast, gen)
         return gen
     
+    def eliminate_dead_code(self):
+        """
+        Function for eliminating dead code in the CFG
+        after constructing the Live Variable sets.
+        """
+        
+    
+    def create_inference_graph(self):
+        """
+        Function for creating the inference graph
+        to help with assigning registers.
+        """
+        # Create the inference graph for variables
+        infer = InferenceGraph()
+        infer.nodes.add("output")
+        for node in self.cfg.nodes:
+            _in = self.IN[node.label]
+            _out = self.OUT[node.label]
+            for var1 in _in:
+                infer.nodes.add(var1)
+                for var2 in _in:
+                    if var1 != var2:
+                        infer.addEdge(var1, var2)
+            
+            for var1 in _out:
+                infer.nodes.add(var1)
+                for var2 in _out:
+                    if var1 != var2:
+                        infer.addEdge(var1, var2)
+        
+        print(f"Inference Graph nodes: {infer.nodes}")
+        print(f"Inference Graph edges: {infer.edges}")
